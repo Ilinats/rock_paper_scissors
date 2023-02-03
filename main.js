993d67901220ec2013ebc12189194d1667ad2bc9
Ilinats <107075825+Ilinats@users.noreply.github.com>
2023-02-03 17:48:42 +0200

    javascript code

---
+++ main.js
@@ -0,0 +1,28 @@
+var playerAnswer;
+var generatedAnswer;
+var result;
+
+const Options = {
+    rock: 0,
+    paper: 1,
+    scissors: 2
+};
+
+
+function saveAnswer(btn) {
+
+    generatedAnswer = Math.floor(Math.random() * 3);
+    playerAnswer = btn.dataset.index;
+
+    if(generatedAnswer == playerAnswer) 
+        result = 0;     //Result equals 0 because there is a draw
+    else if(generatedAnswer == 0 && playerAnswer == 2)
+        result = 2;    //Greater than 1 when computer wins
+    else if(generatedAnswer == 2 && playerAnswer == 0)
+        result = 1;     //1 when player wins
+    else 
+        result = (generatedAnswer > playerAnswer) ? 2 : 1;
+        
+    localStorage.setItem("Result", result);
+    localStorage.setItem("GeneratedAnswer", generatedAnswer);
+}
